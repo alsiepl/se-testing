2d94c385166910de6be2623db8968ab99f347d43
lajoskariko <145137369+lajoskariko@users.noreply.github.com>
2025-03-16 14:24:51 +0100

    Added Examiner class to evaluate student exams and assign grades

--- StudentGradeCalculatorTest/StudentCalculatorTest/obj/Debug/net9.0/StudentCalculatorTest.AssemblyInfo.cs
+++ StudentGradeCalculatorTest/StudentCalculatorTest/obj/Debug/net9.0/StudentCalculatorTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentCalculatorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38783e6f07ced895396729fd797d727d75671015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a335714a3f6f1505bab6d8a7630ce7eff1f148")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentCalculatorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentCalculatorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
